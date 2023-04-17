dba9f249003bf4cece31532cb6d053222dcfac59
JP <jay.patel.py@gmail.com>
2023-04-16 22:21:11 -0600

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "pages/homePage";
+import ProfilePage from "pages/profilePage";
+import LoginPage from "pages/loginPage";
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <LoginPage /> } />
+          <Route path="/home" element={ <HomePage /> } />
+          <Route path="/profile/:userid" element={ <ProfilePage /> } />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
